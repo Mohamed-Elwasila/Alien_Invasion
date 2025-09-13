a9e31f00d2137ef18e774dc87d43c1e90e4c3bb5
Mohamed Elwasila <mhd11.me@gmail.com>
2025-09-13 20:59:54 +0100

    feat: add settings class

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for the game"""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        # Screen settings
+        self.screen_width = 1500
+        self.screen_height = 1000
+        self.bg_color = (180, 180, 180)
